1b131a9d3e5657d3aa8a837668030bd786da1d9f
Rishabh186 <yrishabh94@gmail.com>
2020-08-18 23:43:15 +0530

    make it better

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const bodyparser=require('body-parser');
 const path=require('path')
 
 const PORT=3000;
+
 app.use(bodyparser.urlencoded({extended:false}))
 
 app.use(require('./routes/shop'))
 app.use('/admin',require('./routes/admin'))
 
-
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use((req,res)=>{
     res.status(404).sendFile(path.join(__dirname,'views','error.html'))
