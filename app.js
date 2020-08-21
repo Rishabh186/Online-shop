5f4dc61ee592f783b1a07b2d9d9e17662c5cf2ee
Rishabh186 <yrishabh94@gmail.com>
2020-08-21 23:46:11 +0530

    Created product schema and added update and delete option

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const path = require('path');
-
+// 0rJJNc6iHqh7d4n9
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose=require('mongoose')
 
 const errorController = require('./controllers/error');
 
@@ -20,6 +21,16 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000,()=>{
-    console.log("Connected succesfully")
-});
+mongoose.connect('mongodb+srv://rishabh:0rJJNc6iHqh7d4n9@cluster0.4ynev.mongodb.net/shop?retryWrites=true&w=majority',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(result=>{
+    app.listen(3000,()=>{
+        console.log("Connected succesfully")
+    })
+})
+.catch(error=>{
+    console.log(error);
+})
+
--- models/product.js
+++ models/product.js
@@ -1,40 +1,69 @@
-const fs = require('fs');
-const path = require('path');
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
+const productSchema=new Schema({
+    title:{
+      type:String,
+      required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    imageUrl:{
+      type:String,
+      required:true
+    },
+    description:{
+      type:String,
+      required:true,
     }
-  });
-};
+})
 
-module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
 
-  save() {
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
-  }
+module.exports=mongoose.model('Product',productSchema);
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
-};
+
+
+
+
+// const fs = require('fs');
+// const path = require('path');
+
+// const p = path.join(
+//   path.dirname(process.mainModule.filename),
+//   'data',
+//   'products.json'
+// );
+
+// const getProductsFromFile = cb => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
+
+// module.exports = class Product {
+//   constructor(title, imageUrl, description, price) {
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//   }
+
+//   save() {
+//     getProductsFromFile(products => {
+//       products.push(this);
+//       fs.writeFile(p, JSON.stringify(products), err => {
+//         console.log(err);
+//       });
+//     });
+//   }
+
+//   static fetchAll(cb) {
+//     getProductsFromFile(cb);
+//   }
+// };
