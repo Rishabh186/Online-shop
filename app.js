b998a190192b88f7b1953345f28a1bed4b98f97d
Rishabh186 <yrishabh94@gmail.com>
2020-08-23 18:31:42 +0530

    Added login form

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes=require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,6 +31,7 @@ app.use((req,res,next)=>{
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 const User=require('./models/user')
