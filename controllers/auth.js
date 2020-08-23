e4a2167388857e6ba344062af134058a9ed5a6d7
Rishabh186 <yrishabh94@gmail.com>
2020-08-23 18:13:55 +0530

    Added more css

---
+++ controllers/auth.js
@@ -0,0 +1,14 @@
+
+exports.getLogin=(req,res,next)=>{
+    console.log(req.session.isLoggedin);
+    res.render('auth/login',{
+        path:'/login',
+        pageTitle:'Login',
+        isAuthenticated:false
+    })
+};
+
+exports.postLogin=(req,res,next)=>{
+    req.session.isLoggedin=true,
+    res.redirect('/')
+};
