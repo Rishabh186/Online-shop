fd145ce4640d823983abae997055eb0d796b3dcb
Rishabh186 <yrishabh94@gmail.com>
2020-08-24 03:57:43 +0530

    Added cookies & sessions and make it responsive

--- controllers/error.js
+++ controllers/error.js
@@ -1,4 +1,7 @@
 exports.get404 = (req, res, next) => {
-    res.status(404).render('error', { pageTitle: 'Page Not Found', path: '/404' });
-  };
-  
+  res.status(404).render('error', {
+    pageTitle: 'Page Not Found',
+    path: '/404',
+    isAuthenticated: req.session.isLoggedIn
+  });
+};
