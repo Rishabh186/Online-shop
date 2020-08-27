a4a72ea8ac8d33a93a9bccfdf9e1926fa446a8f1
Rishabh186 <yrishabh94@gmail.com>
2020-08-27 23:53:04 +0530

    Added falsh msges

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
+    errorMessage:req.flash('error')
   });
 };
 
@@ -21,6 +22,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({email:email})
   .then(user=>{
     if(!user){
+      req.flash('error','Invalid email or password')
       return res.redirect('/login');
     }
     bcrypt.compare(password, user.password)
