7231fb949fa0c3d7d1b3b6d7ed01430b0c11e585
Rishabh186 <yrishabh94@gmail.com>
2020-09-03 01:21:29 +0530

    Added validation to signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const nodemailer=require('nodemailer');
 const sendgridTransport=require('nodemailer-sendgrid-transport')
+const {validationResult}=require('express-validator/check')
 const User = require('../models/user');
 
 
@@ -68,33 +69,29 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-Mail already exists!');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then(result => {
-          res.redirect('/login')
-        })
-        .catch(err=>{
-          console.log(err);
-        })
-
-    })
-    .catch(err => {
-      console.log(err);
+  const errors=validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
     });
+  }
+ 
+  bcrypt
+  .hash(password, 12)
+  .then(hashedPassword => {
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      cart: { items: [] }
+    });
+    return user.save();
+  })
+  .then(result => {
+    res.redirect('/login')
+  })
+     
 };
 
 exports.postLogout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const {check, body}=require('express-validator/check')
+
+const User=require('../models/user')
 
 const authController = require('../controllers/auth');
 
@@ -7,7 +10,37 @@ const router = express.Router();
 router.get('/signup',authController.getSignup)
 router.get('/login', authController.getLogin);
 
-router.post('/signup',authController.postSignup)
+router.post('/signup',
+[
+check('email')
+.isEmail()
+.withMessage('Please enter a valid email')
+.custom((value,{req})=>{
+   return User.findOne({ email: value })
+    .then(userDoc => {
+      if (userDoc) {
+        return Promise.reject("E-Mail already exists!");
+      }
+    })
+}),
+
+body(
+    'password',
+    'Please enter a password with alphanumeric characters only and at least 5 characters'
+    )
+.isLength({min:5})
+.isAlphanumeric(),
+
+body('confirmPassword').custom((value,{req})=>{
+    if(value!==req.body.password){
+        throw new Error("Password did not match");
+    }
+    return true;
+})
+],
+authController.postSignup);
+
+
 router.post('/login', authController.postLogin);
 
 
